af9458fe7321d06870da3d20f20953fa8bfb50d1
Nor Edilyan <raul-edil@yandex.ru>
2021-09-26 14:19:03 +0300

    HW OOP

---
+++ OOP_hw1+2.py
@@ -0,0 +1,70 @@
+class Student:
+    def __init__(self, name, surname, gender):
+        self.name = name
+        self.surname = surname
+        self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+    
+    def add_courses(self, course_name):
+        self.finished_courses.append(course_name)  
+
+    def rate_lecturer(self, lecturer, course_2, grade):
+        if isinstance(lecturer, Lecturer) and course_2 in self.courses_in_progress:
+            if course_2 in lecturer.grades:
+                lecturer.grades[course_2] += [grade]
+            else:
+                lecturer.grades[course_2] = [grade]
+        else:
+            return 'Ошибка'
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+        self.grades = {}
+        
+class Lecturer(Mentor):
+    pass
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.courses_in_progress += ['Python']
+ 
+# cool_mentor = Reviewer('Some', 'Buddy')
+# cool_mentor.courses_attached += ['Python']
+ 
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+# cool_mentor.rate_hw(best_student, 'Python', 10)
+ 
+# print(best_student.grades)
+
+best_student = Student('Ruoy', 'Eman', 'your_gender')
+best_student.courses_in_progress += ['Python']
+ 
+cool_mentor = Lecturer('Some', 'Buddy')
+cool_mentor.courses_attached += ['Python']
+
+best_student.rate_lecturer(cool_mentor, 'Python', 10)
+best_student.rate_lecturer(cool_mentor, 'Python', 10)
+best_student.rate_lecturer(cool_mentor, 'Python', 10)
+
+print(cool_mentor.grades)
+
+
+
